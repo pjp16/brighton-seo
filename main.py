4bf826286b6362ccd775a43a27a6807a860cc4ca
pjp16 <71027501+pjp16@users.noreply.github.com>
2021-03-25 14:50:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-st.write('Number of Talks Watched')
+st.write('Time is takes to make tea')
 
 st.write(pd.DataFrame({
-    'Levi': [10],
-    'Phill': [5],
-    'Oli': [1]
+    'Levi': ["2 min"],
+    'Phill': ["30 secs"],
+    'Oli': ["14 hours]
 }))
 
